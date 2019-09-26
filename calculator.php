6219615716e710b70c350f52813c48ee41683e14
KubaKajderowicz <2115394@talnet.nl>
2019-09-26 17:09:11 +0200

    Uitwerking van de exercise

---
+++ calculator.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <form action="calculate.php" method="post">
+            <input type="text" placeholder="eerste num">
+            <input type="text" placeholder="tweede num">
+            <input type="button" text="Add" name="Add">
+            <input type="button" value="Subtract" name="Subtract">
+            <input type="button" value="Multiply" name="Multiply">
+            <input type="button" value="Divide" name="Divide">
+            <input type="button" value="modulo" name="modulo">
+            <input type="submit" value="submit" name="submit">
+        </form>
+    </body>
+</html>
